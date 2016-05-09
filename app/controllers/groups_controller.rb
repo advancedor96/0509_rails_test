80fc68a856dfb95666e2e2cdc5378c032fbb5e59
advancedor96 <advancedor96@gmail.com>
2016-05-09 10:13:18 +0800

    完成第5章了

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,67 @@
 class GroupsController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+	def index
+		@groups = Group.all
+	end
+
+	def show
+		@group = Group.find(params[:id])
+
+		@posts = @group.posts
+
+	end
+
+	def new
+		@group = Group.new
+	end
+
+	def create
+		@group = Group.new(groups_params)
+
+		if @group.save
+			flash[:notice]="新增討論區成功"
+			redirect_to groups_path
+		else
+			flash[:notice]="新增Group...失敗"
+			render :new
+		end
+	end
+
+	def edit
+		@group = Group.find(params[:id])
+
+	end
+
+	def update
+		@group = Group.find(params[:id])
+		if @group.update(groups_params)
+			flash[:notice]="討論區更新成功"
+			redirect_to groups_path
+		else
+			flash[:notice]="更新資料失敗"
+			render :neditew
+		end
+
+	end
+
+	def destroy
+		@group = Group.find(params[:id])
+		@group.destroy
+		flash[:notice]="討論區已刪除"
+		redirect_to groups_path
+
+
+	end
+
+
+	private
+
+	def groups_params
+		params.require(:group).permit(:title, :description)
+	end
+
+
+
+
 end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+	before_action :find_group
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+	def new
+		@post = @group.posts.new
+	end
+
+	def create
+		@post = @group.posts.build(posts_params)
+
+		if @post.save
+			flash[:notice]="文章新增成功"
+			redirect_to group_path(@group)
+		else
+			flash[:notice]="文章新增失敗"
+			render :new
+		end
+
+	end
+
+	def edit
+		@post = @group.posts.find(params[:id])
+
+	end
+
+	def update
+		@post = @group.posts.find(params[:id])
+
+		if @post.update(posts_params)
+			flash[:notice]="文章已更新"
+			redirect_to group_path(@group)
+		else
+			flash[:notice]="文章更新失敗"
+			render :edit
+		end
+
+	end
+
+	def destroy
+		@post = @group.posts.find(params[:id])
+
+		@post.destroy
+		flash[:notice]="文章已刪除"
+		redirect_to group_path(@group)
+
+	end
+
+
+	private
+
+	def find_group
+		@group = Group.find(params[:group_id])
+	end
+
+	def posts_params
+		params.require(:post).permit(:content)
+	end
+
+end
